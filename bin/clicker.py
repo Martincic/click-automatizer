2b7a77c171d980028fe854c88b4c50a4bd0ee801
Martincic <martincic.tomas@gmail.com>
2022-02-05 11:35:45 +0100

    Added safety exit key

--- bin/clicker.py
+++ bin/clicker.py
@@ -1,8 +1,15 @@
+from threading import Thread
 from time import sleep
 import win32api, win32con
 import pyautogui
+import keyboard
+import os
 
 def clickPositions(all_positions, repeat, timeout):
+
+    #Activate safety killswitch
+    thread = Thread(target = safetyKillswitch)
+    thread.start()
     
     should_repeat = True
     iteration = 0
@@ -21,3 +28,8 @@ def click(x,y):
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
 
+def safetyKillswitch():
+    while True:
+        keyboard.wait("esc")
+        print("ESC key pressed! Emergency exit activated... ")
+        os._exit(1)
